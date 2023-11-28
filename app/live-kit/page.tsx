dbf37100b9696446a6cb7d8a7effb4cff518c326
Liu Garnet <mengyehuanyu@gmail.com>
2023-11-28 15:39:18 +0800

    liveKit

--- app/live-kit/page.tsx
+++ app/live-kit/page.tsx
@@ -1,34 +1,48 @@
 "use client";
 
-import {
-  ControlBar,
-  LiveKitRoom,
-  RoomAudioRenderer,
-  useTracks,
-  VideoTrack
-} from "@livekit/components-react";
+import { AudioTrack, LiveKitRoom, useTracks, VideoTrack } from "@livekit/components-react";
+import { useEffect, useState } from "react";
 import { Track } from "livekit-client";
 
+import "@livekit/components-styles";
+
 export default function LiveKitPage() {
-  return (
-    <LiveKitRoom
-      // video={true}
-      // audio={true}
-      token={"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6ImNkYzI0ODQwLWEyNzUtNDBhNy04MGQ4LTQxNjkxMzAxZmQ0NSIsImNhblB1Ymxpc2giOnRydWUsImNhblN1YnNjcmliZSI6dHJ1ZSwiY2FuUHVibGlzaERhdGEiOnRydWV9LCJpYXQiOjE3MDEwNzUxNDEsIm5iZiI6MTcwMTA3NTE0MSwiZXhwIjoxNzAxMDk2NzQxLCJpc3MiOiJBUEluZHh5MkNUSENKVWUiLCJzdWIiOiJkZTllMzQ1MC1jYTFlLTQyMDctOTk3OS0wYTVmYmVkMTkwMTctcHVibGlzaC1wdWJsaXNoIiwianRpIjoiZGU5ZTM0NTAtY2ExZS00MjA3LTk5NzktMGE1ZmJlZDE5MDE3LXB1Ymxpc2gtcHVibGlzaCJ9.AemwbkDTw793ZnCmwiN3IC-5AqtWbsj0fcN9-OxefKQ"}
-      serverUrl={"wss://fanaticslive-staging-nbofaz5v.livekit.cloud"}
-      // Use the default LiveKit theme for nice styles.
-      // data-lk-theme="default"
-      style={{ height: "100vh" }}
-    >
-      {/* Your custom component with basic video conferencing functionality. */}
-      <MyVideoConference/>
-      {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
-      {/*<RoomAudioRenderer/>*/}
-      {/* Controls for the user to start/stop audio, video, and screen
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    fetch("/api/getLiveKitToken", {
+      method: "POST",
+      body: JSON.stringify({
+        user_id: "365920e5-bd87-4499-9c10-bbe84e0b1e33",
+        livestream_id: "cdc24840-a275-40a7-80d8-41691301fd45",
+        identity: "join"
+      })
+    })
+      .then((res) => {
+        console.log(res.ok);
+        return res.json();
+      })
+      .then((res) => {
+        console.log("res", res);
+        setToken(res.token);
+      });
+  }, []);
+
+  if (token) {
+    return (
+      <LiveKitRoom token={token} serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}>
+        {/* Your custom component with basic video conferencing functionality. */}
+        <MyVideoConference/>
+        {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
+        {/*<RoomAudioRenderer/>*/}
+        {/* Controls for the user to start/stop audio, video, and screen
       share tracks and to leave the room. */}
-      {/*<ControlBar/>*/}
-    </LiveKitRoom>
-  );
+        {/*<ControlBar/>*/}
+      </LiveKitRoom>
+    );
+  } else {
+    return null;
+  }
 }
 
 
@@ -37,8 +51,15 @@ function MyVideoConference() {
   // joins without a published camera track, a placeholder track is returned.
   const tracks = useTracks([Track.Source.Unknown, Track.Source.Microphone, Track.Source.Camera]);
 
+  const videoTrack = tracks.find((t) => t.publication.kind === "video");
+  const audioTrack = tracks.find((t) => t.publication.kind === "audio");
   console.log("tracks", tracks);
-  return tracks[0] && <VideoTrack trackRef={tracks[0]}/>;
+  return (
+    <>
+      {!!videoTrack ? <VideoTrack className="!w-[500px]" trackRef={videoTrack}/> : null}
+      {!!audioTrack ? <AudioTrack className="!w-[500px]" trackRef={audioTrack} volume={1}/> : null}
+    </>
+  );
   // return (
   //   <GridLayout tracks={tracks} style={{ height: "calc(100vh - var(--lk-control-bar-height))" }}>
   //     {/* The GridLayout accepts zero or one child. The child is used
