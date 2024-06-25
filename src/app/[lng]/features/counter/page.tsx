686d734e84395bf88a5cd292a8384249290eca3a
Liu Garnet <mengyehuanyu@gmail.com>
2024-06-25 15:49:36 +0800

    feat: add redux

--- src/app/[lng]/features/counter/page.tsx
+++ src/app/[lng]/features/counter/page.tsx
@@ -1,7 +1,8 @@
+import { CounterAction } from "@/app/[lng]/features/counter/libs";
 import { PageHeader } from "@/app/[lng]/features/components/PageHeader/PageHeader";
-import { CounterAction } from "@/app/[lng]/features/counter/components/counter-action/counter-action";
 
 export default async function Page() {
+  console.log("Counter Page");
   return (
     <div className="mx-auto mt-5 w-[1200px]">
       <PageHeader pageTitle="Counter" backRoute="/" />
